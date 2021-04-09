776c16474ae841e2cec5d3f049d6d16f12e8a832
huatxu <andresmsolec@gmail.com>
2021-04-09 23:50:48 +0200

    Feat Front-end: Major refactor

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
 import React, { useState } from "react";
 import "./App.css";
-import { FiltroCervezas } from "./FiltroCervezas";
 import { SelectorIdioma } from "./SelectorIdioma";
 import { ListaCervezas } from "./ListaCervezas";
-
 function App() {
   const [ingles, setIngles] = useState(false);
-  const [artesanales, setArtesanales] = useState(false);
-
+  const [artesanales] = useState(false);
   return (
     <div className={"App"}>
       <header className='App-header'>
         <img src={require("./logo.png")} alt='Erasmus Café' />
         <h2
-          style={{
-            color: "#fff",
-            fontFamily: "Staatliches, sans-serif",
-            fontSize: 35,
-            marginBottom: 0,
-            marginTop: 20,
-          }}>
+          className={'main-title'}>
           {ingles ? "Beer menu" : "Carta de cervezas"}
         </h2>
       </header>
       <SelectorIdioma handleClick={setIngles} ingles={ingles} />
-
-      <FiltroCervezas
-        handleClick={setArtesanales}
-        artesanales={artesanales}
-        ingles={ingles}
-      />
       <ListaCervezas ingles={ingles} artesanales={artesanales} />
       <div
-        style={{
-          color: "white",
-          fontSize: 11,
-          lineBreak: 0,
-          justifySelf: "flex-end",
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-end",
-          marginTop: 20,
-        }}>
-        <p
-          style={{
-            color: "white",
-            fontSize: 11,
-            lineBreak: 0,
-          }}>
-          <span style={{ fontWeight: "bold", color: "#919191" }}>
+        className='footer'>
+        <p>
+          <span style={{ fontWeight: "bold",
+          color:"#919191" }}>
             Erasmus Bruin Café
           </span>{" "}
           © {new Date().getFullYear()}
