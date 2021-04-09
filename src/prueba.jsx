8edc1703820301c472c841138e715847bf59960b
huatxu <andresmsolec@gmail.com>
2021-04-09 23:50:29 +0200

    Improvement: Remove unused assets

--- src/prueba.jsx
+++
@@ -1,7 +0,0 @@
-cervezas
-    .filter((e) => e.tipo === categoriaSeleccionada)
-    .filter(
-        (e) =>
-            (e.artesanal !== "No" && props.artesanales) ||
-            (e.artesanal === "No" && !props.artesanales)
-    ).length === 0
